c685f411ee7488d235612667718d371a9edb2425
Imran2526 <imraninfo97@gmail.com>
2021-02-19 11:54:18 +0530

    [Imran] Add : JWT and USer Registraion and Login Method Added in All Layer

--- BookStoreApplication/Controllers/UserController.cs
+++ BookStoreApplication/Controllers/UserController.cs
@@ -1,1 +1,111 @@
-﻿
+﻿using BookStoreBusinessLayer.IBusinessLayer;
+using BookStoreModelLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : Controller
+    {
+        private readonly IUserBusiness business;
+        private readonly IConfiguration configuration;
+
+        public UserController(IUserBusiness business, IConfiguration configuration)
+        {
+            this.business = business;
+            this.configuration = configuration;
+        }
+
+        /// <summary>
+        /// Genrates the JWT token.
+        /// </summary>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
+        private string GenrateJWTToken(string Email)
+        {
+            var secretkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Key"]));
+            var signinCredentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim("Email",Email)
+            };
+            var tokenOptionOne = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: signinCredentials
+                );
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenOptionOne);
+            return token;
+        }
+
+        /// <summary>
+        /// Register User .
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns></returns>
+
+        [HttpPost]
+        [Route("registerUser")]
+        public IActionResult RegisterUser([FromBody] UserModel user)
+        {
+            try
+            {
+                var result = this.business.UserRegistration(user);
+                if (result != null)
+                {
+                    return this.Ok(new { Status = true, Message = "Data Added Successfully", Data = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "Data is Not Added Succesfully " });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+
+        /// <summary>
+        /// Login the user.
+        /// </summary>
+        /// <param name="login">The login.</param>
+        /// <returns></returns>
+
+        [HttpPost]
+        [Route("loginUser")]
+        public async Task<IActionResult> LoginUser([FromBody] LoginModel login)
+        {
+            try
+            {
+
+                var result = this.business.UserLogin(login);
+                if (result != null)
+                {
+                    var token = GenrateJWTToken(result.Email);
+                    return this.Ok(new { Status = true, Message = "Login Successfully", Data = token });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "Login Failed" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = false, Message = e.Message });
+            }
+        }
+    }
+}
